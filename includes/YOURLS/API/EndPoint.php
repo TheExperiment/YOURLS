0cdaf9bb4fc130a741b7abaf2c14c3d11421fc01
Leo Colombaro <git@colombaro.fr>
2014-04-25 17:00:13 +0200

    Interfaces EndPoint for API

---
+++ includes/YOURLS/API/EndPoint.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * EndPoint
+ *
+ * @since 2.0
+ * @version 2.0-alpha
+ * @copyright 2009-2014 YOURLS
+ * @license MIT
+ */
+
+namespace YOURLS\API;
+
+interface EndPoint extends Answer {
+
+    public function __construct() {}
+
+}
