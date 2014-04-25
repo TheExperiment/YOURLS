9349659c00eba03aa9fdd00e7a9e750eaeede20a
Leo Colombaro <git@colombaro.fr>
2014-04-25 19:27:04 +0200

    Add my tests even if it fails

--- tests/YOURLS/Utilities/FormatTest.php
+++ tests/YOURLS/Utilities/FormatTest.php
@@ -7,7 +7,7 @@
  * @copyright 2009-2014 YOURLS
  * @license MIT
  */
- 
+
 namespace YOURLS\Utilities;
 
 class FormatTest extends \PHPUnit_Framework_TestCase {
@@ -15,8 +15,37 @@ class FormatTest extends \PHPUnit_Framework_TestCase {
     /**
      * @covers YOURLS\Utilities\Format::int2string
      */
-    public function testInt2string() {
-        $this->assertEquals( '115', Format::int2string( 1337, '0123456789abcdefghijklmnopqrstuvwxyz' ) );
+    public function test_int2string() {
+        $this->assertEquals( 'lz', Format::int2string( 1337, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ' ) );
     }
-    
+
+    /**
+     * @covers YOURLS\Utilities\Format::string2int
+     */
+    public function test_string2int() {
+        $this->assertEquals( 12730, Format::string2int( '3jk', '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ' ) );
+    }
+
+    /**
+     * @covers YOURLS\Utilities\Format::string2htmlid
+     */
+    public function test_string2htmlid() {
+        $this->assertEquals( 'y1667525113', Format::string2htmlid( '3jkdfw f166aé 165è(z' ) );
+    }
+
+    /**
+     * @covers YOURLS\Utilities\Format::sanitize_string
+     */
+    public function test_sanitize_string() {
+        $this->markTestSkipped();
+        $this->assertEquals( 'y1667525113', Format::sanitize_string( '3jkdfw f166aé 165è(z' ) );
+    }
+
+    /**
+     * @covers YOURLS\Utilities\Format::sanitize_title
+     */
+    public function test_sanitize_title() {
+        $this->assertEquals( 'Vized@sd&400;g "ofi ed799 (', Format::sanitize_title( 'Vized@sd&400;g "ofi ed799 (' ) );
+    }
+
 }
