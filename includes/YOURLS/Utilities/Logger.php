118829010f585ea8e72edf12ee8c8a4f8f294a0e
Leo Colombaro <git@colombaro.fr>
2014-05-03 17:32:44 +0200

    Log more information such as Git env. and Network info

--- includes/YOURLS/Utilities/Logger.php
+++ includes/YOURLS/Utilities/Logger.php
@@ -12,6 +12,8 @@
 namespace YOURLS\Utilities;
 
 use Monolog\Handler\StreamHandler;
+use Monolog\Handler\ErrorHandler;
+use Monolog\Processor\WebProcessor;
 
 /**
  * Construct a logger engine
@@ -22,9 +24,11 @@ class Logger extends \Monolog\Logger {
      * @param string $channel The name of the channel
      */
     public function __construct( $channel ) {
-        parent::__construct( 'YOURLS.' . $channel );
-        if ( defined( 'YOURLS_DEBUG' ) && YOURLS_DEBUG == true ) {
+        parent::__construct( 'YOURLS.' . strtoupper( $channel ) );
+        $this->pushHandler( new ErrorHandler() );
+        if ( Configuration::is( 'debug' ) ) {
             $this->pushHandler( new StreamHandler( YOURLS_USERDIR . '/yourls.log' ) );
+            $this->pushProcessor( new WebProcessor );
         }
     }
 }
