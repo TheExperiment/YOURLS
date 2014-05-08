4673e130642559ae40398d1d82beaa174b1474b7
Leo Colombaro <git@colombaro.fr>
2014-05-08 19:37:03 +0200

    This is what I want
    Draft API\Request construction

--- includes/YOURLS/API/Request.php
+++ includes/YOURLS/API/Request.php
@@ -12,6 +12,25 @@
 namespace YOURLS\API;
 
 class Request {
+    
+    public $actions = array();
 
+    public function __construct() {
+        Filters::apply_filters( 'api_actions', $this->actions );
+        
+        $action = isset( $_REQUEST['action'] ) ? $_REQUEST['action'] : null;
+        Filters::do_action( 'api', $action );
+        
+        try {
+            new Answer( new $this->actions[$action] );
+        } catch (Exception $e) {
+            new Answer( array(
+                'status_code' => 400,
+                'message'   => 'Unknown or missing "action" parameter',
+                'simple'    => 'Unknown or missing "action" parameter',
+            ) );
+        }
+     
+    }
 
 }
